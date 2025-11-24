b6fb138b94760400a36ab45081dd791aa556e841
keertana435 <keertanaramesh0@gmail.com>
2025-11-23 19:38:51 -0800

    Add files via upload

---
+++ compare two numbers entered by the user to check which is greater, smaller or equal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("Enter two numbers:");
+scanf("%d%d",&a,&b);
+if(a==b)
+{
+    printf("%d is eqaul to %d\n",a,b);
+}
+else if (a>b)
+{
+    printf("%d is greater than %d\n",a,b);
+}
+else if (a<b)
+{
+    printf("%d is smaller than %d\n",a,b);
+}
+  return 0;
+}
+
